a555711d81eb0ed92c272c98e9564fad667c230d
Tchannikov Daniel <75910357+Daniel-Developer-p@users.noreply.github.com>
2023-06-29 12:57:57 +0300

    just commit for push

---
+++ MVP-SinsStuff/MVP/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  MVP-SinsStuff
+//
+//  Created by Даниил Тчанников on 28.06.2023.
+//
+
+import Foundation
+
+struct User: Codable {
+    let name: String
+    let email: String
+    let username: String
+}
